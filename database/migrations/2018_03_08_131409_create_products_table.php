888cb162d7d7f4a7c651e3b7bc957e01a131a406
Usman Akram <usman.akram@gmail.com>
2018-03-08 20:31:47 +0500

    Revert "price added"
    This reverts commit 39c8b3156ed4d93dbabadb0b0a6ba77b7e2a8c31.

--- database/migrations/2018_03_08_131409_create_products_table.php
+++ database/migrations/2018_03_08_131409_create_products_table.php
@@ -17,7 +17,6 @@ class CreateProductsTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->text('description');
-            $table->integer('price');
             $table->timestamps();
         });
     }
