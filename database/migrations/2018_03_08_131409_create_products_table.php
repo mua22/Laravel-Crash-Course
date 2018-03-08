39c8b3156ed4d93dbabadb0b0a6ba77b7e2a8c31
Usman Akram <usman.akram@gmail.com>
2018-03-08 20:30:09 +0500

    price added

--- database/migrations/2018_03_08_131409_create_products_table.php
+++ database/migrations/2018_03_08_131409_create_products_table.php
@@ -17,6 +17,7 @@ class CreateProductsTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->text('description');
+            $table->integer('price');
             $table->timestamps();
         });
     }
